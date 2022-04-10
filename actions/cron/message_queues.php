b73291875346912c59d79afa93038bcbfdf8e3cd
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2022-04-11 00:36:35 +0700

    add limit queue

--- actions/cron/message_queues.php
+++ actions/cron/message_queues.php
@@ -3,17 +3,22 @@
 $conn = conn();
 $db   = new Database($conn);
 
-$queue = $db->single('message_queues');
-$message = $db->single('messages',[
-    'id' => $queue->message_id
-]);
-
-$attachments = $db->all('message_attachments',[
-    'message_id' => $queue->message_id  
-]);
-
-sendMail($message->title,[$queue->email],$queue->message_content,$attachments);
-
-$db->delete('message_queues',[
-    'id' => $queue->id
-]);
+$query = "SELECT * FROM message_queues LIMIT 0, 15";
+$db->query = $query;
+$queues = $db->exec('all');
+foreach($queues as $queue)
+{
+    $message = $db->single('messages',[
+        'id' => $queue->message_id
+    ]);
+    
+    $attachments = $db->all('message_attachments',[
+        'message_id' => $queue->message_id  
+    ]);
+    
+    sendMail($message->title,[$queue->email],$queue->message_content,$attachments);
+    
+    $db->delete('message_queues',[
+        'id' => $queue->id
+    ]);
+}
